beef21c4491094232b4eb1c28524f00203168f43
Calvin Kwong <ckwong93@gmail.com>
2017-03-30 22:05:17 -0700

    add components folder to basic wireframe - app, signIn, signUp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './components/App';
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<div>App</div>,document.getElementById('root'))
