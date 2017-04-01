5652b81f15a7181735590afb65dad4faf0e2aa5f
Calvin Kwong <ckwong93@gmail.com>
2017-04-01 15:50:52 -0700

    create link to firebase via completeGoalRef...on click of completion button, data gets saved to new database with current user email and goal title

--- src/components/GoalItem.jsx
+++ src/components/GoalItem.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { completeGoalRef } from '../firebase';
+
 
 class GoalItem extends Component {
+  completeGoal(){
+    // add to complete goals on the database
+    //remove this goal from goals reference
+    const { email } = this.props.user;
+    const { title } = this.props.goal;
+    // console.log('email', email, 'title', title);
+    completeGoalRef.push({email, title});
+    // when user clicks complete, this pushes the item to the completed goal database
+  }
   render(){
     const {email, title} = this.props.goal;
     return(
       <div style={{margin: '5px'}}>
         <strong>{title}</strong>
-        <span> submitted by <em>{email}</em></span>
+        <span style={{marginRight: '5px'}}> submitted by <em>{email}</em></span>
+        <button
+          className='btn btn-sm btn-primary'
+          onClick={() => this.completeGoal()}
+          >
+            Complete
+          </button>
       </div>
     )
   }
 }
 
-export default GoalItem;
+function mapStateToProps(state){
+  const { user } = state;
+  return {
+    user
+  }
+}
+export default connect(mapStateToProps, null)(GoalItem);
