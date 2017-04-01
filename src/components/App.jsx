65ca2ab7f11650d26e1c302078519ae536697fde
Calvin Kwong <ckwong93@gmail.com>
2017-04-01 13:20:17 -0700

    connect redux to firebase, allowing us to save user info to store

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { firebaseApp } from '../firebase';
 
 
@@ -21,4 +22,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state){
+  console.log('state', state);
+  return{}
+}
+export default connect (mapStateToProps, null)(App);
